9f2ab1d83d6722e87825035e5fe669dda0585739
santhiya-it19 <santhiya.it19@bitsathy.ac.in>
2019-08-28 09:45:49 +0530

    thirty commit

---
+++ bio.php
@@ -0,0 +1,56 @@
+<html>
+<body style="background-image:url(http://www.portoalegre.travel/upload/b/121/1$
+background-color:lavender;font-size:20px;">
+<h1 style="text-align:center;font-family:algerian;">
+BIODATA
+</h1>
+<b>
+<pre>
+1.NAME                 :Santhiya.R<br>
+2.DOB                  :17-02-2002<br>
+3.GENDER               :female<br>
+4.FATHER'S NAME        :Rajenderan.S<br>
+5.BLOOD GROUP          :o+ve<br>
+6.ADDRESS              :26/46 F,upputhottam,<br>
+                        avinasi rd,<br>
+                        annur,<br>
+7.HOBBIES               :
+<ul>
+<li>readingbooks</li>
+<li>listeningmusic</li>
+</ul><br>
+8.RELIGION              :Hindhu<br>
+9.NATIONALITY           :Indian<br>
+10.AGE                  :17<br>
+11.KNOWN LANGUAGES      :tamil & english<br>
+12.QUALIFICATION        :
+ <table border="1" style="font-size:20px;font-family:algerian;">
+ <b>
+<tr>
+<td> degree </td>
+<td> institution </td>
+<td> institution </td>
+<td> year of passing </td>
+</tr>
+<tr>
+<td> 10 th </td>
+<td> st.marys,annur </td>
+<td> 2016-2017 </td>
+</tr>
+<tr>
+<td> 12 th </td>
+<td> st.marys,annur </td>
+<td> 2018-2019 </td>
+</tr>
+
+ <tr>
+<td> B.Tech </td>
+<td> BIT,sathy </td>
+<td> 2019-2023 </td>
+</tr>
+ </table></b><br>
+</pre>
+</b>
+</body>
+</html>
+
